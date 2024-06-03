2ff6634ec4b231c49d92bc6a2a695b3240159955
Alan Henrique <alanhipermassa@gmail.com>
2024-06-03 20:56:31 -0300

    Adicionados atributos e relacionamentos da classe Item_Venda

--- src/main/java/br/unipar/programacaointernet/pdvweb/model/Item_Venda.java
+++ src/main/java/br/unipar/programacaointernet/pdvweb/model/Item_Venda.java
@@ -1,4 +1,27 @@
 package br.unipar.programacaointernet.pdvweb.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Item_Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private int quantidade;
+
+    private Double valor_unitario;
+
+    private Double valor_total;
+
+    @ManyToOne
+    private Venda venda;
+
+    @ManyToOne
+    private Produto produto;
 }
