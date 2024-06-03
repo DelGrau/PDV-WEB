e791b37e593d2ffd16274e8aa395afcbdad27cec
Alan Henrique <alanhipermassa@gmail.com>
2024-06-03 20:57:35 -0300

    Adicionados atributos da classe Produto

--- src/main/java/br/unipar/programacaointernet/pdvweb/model/Produto.java
+++ src/main/java/br/unipar/programacaointernet/pdvweb/model/Produto.java
@@ -1,4 +1,32 @@
 package br.unipar.programacaointernet.pdvweb.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String descricao;
+
+    private double valor;
+
+    /**
+     * CATEGORIA
+     * O tipo do produto.
+     * EX:
+     * - Loja de Roupas: Camisas, Blusas, Calças...
+     * - Loja de Eletronicos: Perifericos, Placa de Vídeo...
+     */
+    private String categoria;
+
 }
