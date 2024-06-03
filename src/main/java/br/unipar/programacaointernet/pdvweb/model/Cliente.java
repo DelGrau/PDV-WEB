7167a2215e028bf4769177debec77ac74c4a5d5f
Alan Henrique <alanhipermassa@gmail.com>
2024-06-03 20:56:04 -0300

    Adicionados atributos da classe Cliente

--- src/main/java/br/unipar/programacaointernet/pdvweb/model/Cliente.java
+++ src/main/java/br/unipar/programacaointernet/pdvweb/model/Cliente.java
@@ -1,4 +1,25 @@
 package br.unipar.programacaointernet.pdvweb.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Repository;
+
+@Getter
+@Setter
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String nome;
+
+    private String telefone;
+
+    private String email;
 }
