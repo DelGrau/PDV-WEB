0ef7714df2973be5c56e12241733bcb828a78a7a
Alan Henrique <alanhipermassa@gmail.com>
2024-06-03 20:57:15 -0300

    Adicionados atributos e relacionamentos da classe Venda

--- src/main/java/br/unipar/programacaointernet/pdvweb/model/Venda.java
+++ src/main/java/br/unipar/programacaointernet/pdvweb/model/Venda.java
@@ -1,4 +1,33 @@
 package br.unipar.programacaointernet.pdvweb.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import net.sf.jsqlparser.expression.DateTimeLiteralExpression;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
 public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String observacoes;
+
+    private Date data;
+
+    private double total;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "venda",      // Diz que está baseado no atributo venda da classe Item_venda
+            fetch = FetchType.EAGER,    // Traz todos os Item_venda quando carregar a venda
+            orphanRemoval = true)       // Apaga os Item_venda quando a Venda for Excluida
+    private List<Item_Venda> item_venda;
 }
