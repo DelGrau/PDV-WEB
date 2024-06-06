05be500ef651892ded9d90b5d35d6a3da469f35c
Felipe Talini <felipetalini5@gmail.com>
2024-06-06 19:53:47 -0300

    correcao
    correção do texto

--- src/main/java/br/unipar/programacaointernet/pdvweb/controller/ClienteController.java
+++ src/main/java/br/unipar/programacaointernet/pdvweb/controller/ClienteController.java
@@ -4,11 +4,13 @@ import br.unipar.programacaointernet.pdvweb.Service.ClienteService;
 import br.unipar.programacaointernet.pdvweb.model.Cliente;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.Path;
 import java.util.List;
-@Tag(name = "/ClienteController")
-@RestController
+@Tag(name = "/clientecontrollerapi")
+@Controller
 public class ClienteController {
     private final ClienteService clienteService;
 
@@ -16,22 +18,22 @@ public class ClienteController {
         this.clienteService = clienteService;
     }
 
-    @GetMapping(path = "/All")
+    @GetMapping(path = "/all")
     public ResponseEntity<List<Cliente>> GetAll(){
         return ResponseEntity.ok(clienteService.Todos());
     }
 
-    @PostMapping(path = "/GetById")
+    @PostMapping(path = "/getbyid")
     public  ResponseEntity<Cliente> GetById(@RequestBody Integer id){
         return ResponseEntity.ok(clienteService.buscarPorId(id));
     }
-    @DeleteMapping(path = "/Delete")
+    @DeleteMapping(path = "/delete")
     public ResponseEntity Delete(@RequestBody Cliente cliente)
     {
         clienteService.buscarPorId(cliente.getId());
         return ResponseEntity.ok("Cliente "+cliente.getNome()+" deletado com sucesso!");
     }
-    @PostMapping(path = "/Save")
+    @PostMapping(path = "/save")
     public ResponseEntity Save(@RequestBody Cliente cliente){
         if(clienteService.buscarPorId(cliente.getId()) == null){
             Cliente cli =  new Cliente();
