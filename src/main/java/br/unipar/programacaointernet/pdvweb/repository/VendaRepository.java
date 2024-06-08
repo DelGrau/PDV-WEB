6bcf9f78c8f151c952dc741999615ee2e034ad4e
DelGrau <alanhipermassa@gmail.com>
2024-06-08 16:38:38 -0300

    Removidas funções que impediam programa de iniciar

--- src/main/java/br/unipar/programacaointernet/pdvweb/repository/VendaRepository.java
+++ src/main/java/br/unipar/programacaointernet/pdvweb/repository/VendaRepository.java
@@ -6,10 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
-    List<Venda> findAllVendas();
     Venda findVendaById(Integer id);
-    Venda saveVenda(Venda venda);
-    Venda updateVenda(Venda venda);
-    void deleteVenda(Venda venda);
-
 }
