1a703dd53cbd0561753459377cae4d9deaffb2a8
DelGrau <alanhipermassa@gmail.com>
2024-06-09 23:37:09 -0300

    FindVendaByData

--- src/main/java/br/unipar/programacaointernet/pdvweb/repository/VendaRepository.java
+++ src/main/java/br/unipar/programacaointernet/pdvweb/repository/VendaRepository.java
@@ -2,9 +2,14 @@ package br.unipar.programacaointernet.pdvweb.repository;
 
 import br.unipar.programacaointernet.pdvweb.model.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
     Venda findVendaById(Integer id);
+
+    @Query(value = "SELECT v.id, v.observacoes, v.data, v.total, v.cliente, v.item_venda FROM Venda v WHERE v.data BETWEEN ?1 AND ?2")
+    List<Venda> findVendaByData(LocalDate dataInicial, LocalDate dataFinal);
 }
